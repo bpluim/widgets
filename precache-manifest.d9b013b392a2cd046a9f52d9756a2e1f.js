f68de152d3a07baa15fb31ddcef559c2cd1a6461
Ben Pluim <bpluim@Administrators-MacBook-Pro.local>
2020-10-21 16:12:15 -0600

    Updates

---
+++ precache-manifest.d9b013b392a2cd046a9f52d9756a2e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ffb3852ebfe25e3a0bb6e422cac79c",
+    "url": "/widgets/index.html"
+  },
+  {
+    "revision": "860ab5e0b933792ba57c",
+    "url": "/widgets/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6515d178a098c9e52dae",
+    "url": "/widgets/static/js/2.001fc8ef.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/widgets/static/js/2.001fc8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860ab5e0b933792ba57c",
+    "url": "/widgets/static/js/main.8fdc49ec.chunk.js"
+  },
+  {
+    "revision": "a3b7248df9ec66a8e8f5",
+    "url": "/widgets/static/js/runtime-main.4d12c3f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/widgets/static/media/logo.5d5d9eef.svg"
+  }
+]);
